a7daad4c41b32e7e2ebee31e203a2c0dbd8f1fb3
BhawnaBidlan <109671441+BhawnaBidlan@users.noreply.github.com>
2023-10-04 15:19:47 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,24 @@
+// Initialize the display value
+let displayValue = "";
+
+// Function to append a character to the display
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById("display").value = displayValue;
+}
+
+// Function to clear the display
+function clearDisplay() {
+    displayValue = "";
+    document.getElementById("display").value = displayValue;
+}
+
+// Function to calculate the result
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById("display").value = displayValue;
+    } catch (error) {
+        document.getElementById("display").value = "Error";
+    }
+}
---
+++ Landing Page/README.md
@@ -0,0 +1,1 @@
+# Asian-Dynasty-Restaurant-Landing-Page
